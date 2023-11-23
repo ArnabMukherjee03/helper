789169c9137a89bf765f31fc7ac484ce83367a42
Arnab Mukherjee <arnabmukherjee58260@gmail.com>
2023-11-24 00:39:40 +0530

    Deployment Update

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ connection();
 // Your code
 if (process.env.NODE_ENV === "production") {
     const path = require("path");
-    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
-    app.get("*", (req, res) => {
+    server.use(express.static(path.resolve(__dirname, 'client', 'build')));
+    server.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
             if(err) {
                 res.status(500).send(err)
