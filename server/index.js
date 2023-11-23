440d408b9dff77ac64b2e13edd57f05681040ab6
Arnab Mukherjee <arnabmukherjee58260@gmail.com>
2023-11-24 00:27:14 +0530

    Ready For Deployment

--- server/index.js
+++ server/index.js
@@ -7,6 +7,21 @@ const port = process.env.PORT;
 //* DataBase Connection
 connection();
 
+// Your code
+if (process.env.NODE_ENV === "production") {
+    const path = require("path");
+    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+            if(err) {
+                res.status(500).send(err)
+            }
+        });
+    })
+}
+// Your code
+
+
 
 //* Server side Port
  const socket = server.listen(port,()=>{
